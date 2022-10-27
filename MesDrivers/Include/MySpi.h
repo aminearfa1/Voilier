31f9d03f61b3ce67aff17f10113f5ce9d7100120
josephvelazcordz <josephvelazco@gmail.com>
2022-10-27 15:26:37 +0200

    Driver spi

---
+++ MesDrivers/Include/MySpi.h
@@ -0,0 +1,16 @@
+#include <stm32f10x.h>
+
+
+void SPIConfig(void);
+void SPI_Transmit(uint8_t *data,int size);
+void SPI_Receive(uint8_t *data, int size);
+void SPI_Enable (void);
+void SPI_Disable (void);
+void CS_Enable (void);
+void CS_Disable (void);
+void adxl_write (uint8_t address, uint8_t value);
+void adxl_read (uint8_t address);
+void adxl_init (void);
+
+float xg, yg, zg;
+int16_t x,y,z;
---
+++ Projet_Anti_chavirement/main.c
@@ -0,0 +1,18 @@
+#include "MyGPIO.h"
+#include "MyTimer.h"
+#include <stm32f10x.h>
+
+
+int main (void)
+{
+	
+	
+	
+	
+	
+	
+	while(1)
+		{
+			
+		}
+}
