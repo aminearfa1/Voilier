aa04a2d3cfb0a13d1f311195487f7fdc5d489f6c
aminearfa1 <58252765+aminearfa1@users.noreply.github.com>
2022-10-27 19:16:13 +0200

    Update MyUsart.c

--- MesDrivers/Source/MyUsart.c
+++ MesDrivers/Source/MyUsart.c
@@ -2,12 +2,27 @@
 #include "MyGPIO.h"
 #include "stm32f10x.h"
 
+MyGPIO_Struct_TypeDef tx;
+MyGPIO_Struct_TypeDef Rx;
 
 //structure d'initialisation Usart
 void My_Usart_init(USART_TypeDef * UART){
 	
 	if (UART == USART1) {
 	RCC->APB2ENR |= RCC_APB2ENR_USART1EN ;
+	//Gpio init tx 
+	tx.GPIO=GPIOA;
+	tx.GPIO_Conf=Out_PushPull;
+	tx.GPIO_Pin=9;
+	MyGPIO_Init(&tx);
+	
+	//Gpio init rx
+	rx.GPIO=GPIOA;
+	rx.GPIO_Conf=Out_PushPull;
+	rx.GPIO_Pin=10;
+	MyGPIO_Init(&rx);
+	
+	
 	USART1->BRR |= (int) 72000000/(9600) ; //Fréquence h USART1 = 72MHz
 	USART1->CR1|= USART_CR1_TE; //USART Tx
 	USART1->CR1 |= USART_CR1_RE; //USART Rx
