f2e2a0e8a049a43443098f27bc008400d639d417
aminearfa1 <58252765+aminearfa1@users.noreply.github.com>
2022-11-02 13:57:08 +0100

    V1 (gestion du plateau)
    (à tester avec uart

--- Fonctionnalités/source/Plateau.c
+++ Fonctionnalités/source/Plateau.c
@@ -6,19 +6,19 @@
 
 void Plateau_Init(void){
 	// PWM 
-	GPIO_Init();
+	GPIO_Init(GPIOB, 6, AltOut_Ppull_2MHZ);
 	// Bit de sens 
-	GPIO_Init();
+	GPIO_Init(GPIOC, 7, Out_Ppull_2MHZ);
 	
 	// Initialisation du Timer 4 (20kHz)
-	Timer_Base_Init();
+	Timer_Base_Init(TIM4, ,0);
 	
 	// Initialisation de la PWM sur TIM4 & Channel 1
-	Timer_PWM();
+	MyTimer_PWM_Init(TIM4, 1);
 	
 	// Etablissement du rapport cyclique à 0%
-	Timer_Set_Cycle_PWM();
+	MyTimer_PWM_ConfigureRatio(TIM4,1, 0);
 	
 	// Lancement du compteur
-	Timer_Base_Start();
+	Timer_Base_Start(TIM4);
 }	
