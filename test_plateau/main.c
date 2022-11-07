a50226d9a8812041343c48de3e936d5d961d9988
aminearfa1 <58252765+aminearfa1@users.noreply.github.com>
2022-11-07 17:50:44 +0100

    "test plateau en cours"

--- test_plateau/main.c
+++ test_plateau/main.c
@@ -1,8 +1,23 @@
+#include "stm32f10x.h"
+#include "MyGPIO.h"
+#include "MyUsart.h"
+#include "MyTimer.h"
+#include "Plateau.h"
 
 
+signed char val = 0;
+float vf;
+
+void vitesse_modifier() {
+	valeur = MyUART_Receive(UART) ;
+	
+}
 //test plateau
 int main (){
 
-	void Plateau_Init(void);
-
+	Plateau_Init(vitesse_modifier);
+	do {
+		
+		
+	} while(1) ;
 }
