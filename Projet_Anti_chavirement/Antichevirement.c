9a3cfc67312bd99687eeca2efe5d093262196fb3
josephvelazcordz <josephvelazco@gmail.com>
2022-11-17 12:01:24 +0100

    SPI Librairie

--- Projet_Anti_chavirement/Antichevirement.c
+++ Projet_Anti_chavirement/Antichevirement.c
@@ -6,56 +6,26 @@
 
 
 
+
 int main (void)
 {
-	int x,y,z;
-	double xg, yg, zg;
+	char message;
+	MyGPIO_Init(GPIOA,4,Out_PushPull); //NSS CS
+	MyGPIO_Init(GPIOA,5,AltOut_PushPull); //SCK
+	MyGPIO_Init(GPIOA,6,In_Floating); //MISO
+	MyGPIO_Init(GPIOA,7,AltOut_PushPull); //MOSI
 	
+	MySPI_Init(SPI1);
 	
-	MyGPIO_Struct_TypeDef NSS;
-	MyGPIO_Struct_TypeDef SCK;
-	MyGPIO_Struct_TypeDef MISO;
-	MyGPIO_Struct_TypeDef MOSI;
-	
-	NSS.GPIO=GPIOA;
-	NSS.GPIO_Conf=Out_OpenDrain;
-	NSS.GPIO_Pin=4;
-	
-	SCK.GPIO=GPIOA;
-	SCK.GPIO_Conf=AltOut_PushPull;
-	SCK.GPIO_Pin=5;
-	
-	
-	MISO.GPIO=GPIOA;
-	MISO.GPIO_Conf=In_Floating;
-	MISO.GPIO_Pin=6;
-	
-	
-	MOSI.GPIO=GPIOA;
-	MOSI.GPIO_Conf=AltOut_PushPull;
-	MOSI.GPIO_Pin=7;
-	
-	MyGPIO_Init(&NSS);
-	MyGPIO_Init(&SCK);
-	MyGPIO_Init(&MISO);
-	MyGPIO_Init(&MOSI);
-	
-	SPIConfig();
-	
-	SPI_Enable();
-	
-	adxl_init();
 	
 	while(1)
 		{
-		adxl_read (0x32);		
-		x = ((RxData[1]<<8)|RxData[0]);
-		y = ((RxData[3]<<8)|RxData[2]);
-		z = ((RxData[5]<<8)|RxData[4]);
-
-	  xg = x*.0078;
-    yg = y*.0078;
-   	zg = z*.0078;
+			MySPI_Clear_NSS();
+			//MySPI_Send();
+			message=MySPI_Read();
+			MySPI_Set_NSS();
+			
+			
 		}
 }
 
