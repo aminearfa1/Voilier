862bc5d3dd0d752c6e6aeb988de1af3cf7468f12
Herstory20 <bzhong@insa-toulouse.fr>
2022-11-25 16:42:10 +0100

    Servo rajouter et à tester

---
+++ Fonctionnalité/Include/Voile.h
@@ -0,0 +1,13 @@
+#include "stm32f10x.h"
+#include "MyTimer.h"
+#include "MyGPIO.h"
+#include "Girouette.h"
+
+
+void Voile_Init(TIM_TypeDef *Timer, GPIO_TypeDef * GPIO, char Pin) ;
+
+int Voile_AngletoRatio(int angle);
+
+int Voile_AngleRameneDansIntervalle (int angle) ;
+
+void Voile_Set_RatioPWM (int angle);
--- Projet_bordage_auto/main.c
+++ Projet_bordage_auto/main.c
@@ -2,13 +2,16 @@
 #include "MyTimer.h"
 #include "MyGPIO.h"
 #include "Girouette.h"
-
+#include "Voile.h"
+float a;
 int main (){
 	
-	Girouette_Init (TIM2,GPIOA,1,2);
+	Girouette_Init (TIM2,GPIOA,0,1);
+	Voile_Init(TIM3, GPIOB,1);
 	
 	while (1) {
-		float a =  Girouette_Convert(TIM2);
+		//a =  Girouette_Convert(TIM2);
+		Voile_Set_RatioPWM(Voile_AngleRameneDansIntervalle(a));
 	}
 	
 }
