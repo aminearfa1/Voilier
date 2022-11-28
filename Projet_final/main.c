f1dddc42a07ea704b8100a6c0535a4c5e148fc03
aminearfa1 <58252765+aminearfa1@users.noreply.github.com>
2022-11-28 19:13:07 +0100

    first test

--- Projet_final/main.c
+++ Projet_final/main.c
@@ -1,25 +1,88 @@
 #include "MyUsart.h"
 #include "MyGPIO.h"
+#include "Batterie.h"
 #include "stm32f10x.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "SysTick.h"
+#include "Plateau.h"
+#include "MyTimer.h"
+#include "Girouette.h"
+#include "Voile.h"
+
+char Data;
+int vitesse=0;
+float a;
+
+void CallbackV(){
+	Data = Usart_rx(USART1);
+	//debut de l'écout sur USART1.
+			vitesse = (int)((signed char)Data);
+			//Commande à éxecuter.
+			if ( vitesse <= 0)  {
+				MyGPIO_Set(GPIOC,7);
+				//setCycle_PWM(TIM4,1,(-1)*vitesse);
+				MyTimer_PWM_ConfigureRatio(TIM4,1,(-1)*vitesse);
+			}
+			else {
+				MyGPIO_Reset(GPIOC,7);
+				//setCycle_PWM(TIM4,1,vitesse);
+				MyTimer_PWM_ConfigureRatio(TIM4,1,vitesse);
+			}
+}
+/*
+int Cpt_Tourniquet;
+void Tourniquet(void)  // 100ms
+{
+	Cpt_Tourniquet=(Cpt_Tourniquet+1)%100;
+	
+	if ((Cpt_Tourniquet%10)==0)
+	{
+		
+	}
+	
+}
+*/
+
 
 //test USART
 char info;
+
+
+void inter(void){
+	if(Is_Batterie_Faible()){
+	Send_Message("Attention Batteri Faible\n");
+	}
+	
+}
+
 
 void callback(){
 info = Usart_rx(USART1);
 }
 
 int main (){
+	
+Plateau_Init();
+//Récupération des commandes du plateau transmise par la télécommande
+MyUART_ActiveIT(USART1,1, CallbackV);
+	
+Send_Message("Usart Fonctionnel\n");
+
+Batterie_Init();
 
 My_Usart_init(USART1);
+Girouette_Init (TIM2,GPIOA,0,1);
+Voile_Init(TIM3, GPIOB,1);
 	
 	
-MyUART_ActiveIT(USART1,1, callback);
+//tout les 2 seconde
+SysTick_Init(0, 20, (*inter));
 	
-Usart_tx(USART1, "usart fini");
-
 while(1){ 
+	
+	a =  Girouette_Convert(TIM2);
+	Voile_Set_RatioPWM(Voile_AngleRameneDansIntervalle(a));
 	
 	}
 }
-
