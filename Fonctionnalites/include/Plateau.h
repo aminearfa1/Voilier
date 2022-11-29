3f746163212f34cf3c23377d0bc37f9496bed7e9
Herstory20 <bzhong@insa-toulouse.fr>
2022-11-29 13:30:45 +0100

    projet final en cours 2

---
+++ Fonctionnalites/include/Plateau.h
@@ -0,0 +1,1 @@
+void Plateau_Init();
---
+++ Fonctionnalites/source/Plateau.c
@@ -0,0 +1,24 @@
+#include "stm32f10x.h"
+#include "MyGPIO.h"
+#include "MyTimer.h"
+#include "MyUsart.h"
+
+void Plateau_Init(){
+	// PWM 
+	MyGPIO_Init(GPIOB, 6, AltOut_PushPull);
+	// Bit de sens 
+	MyGPIO_Init(GPIOC, 7, Out_PushPull);
+	
+	// Initialisation du Timer 4 à (20kHz)
+	MyTimer_Base_Init(TIM4,3599 ,0);
+	
+	// Initialisation de la PWM sur TIM4 & Channel 1
+	MyTimer_PWM_Init(TIM4, 1);
+	
+	// Etablissement du rapport cyclique à 0%
+	MyTimer_PWM_ConfigureRatio(TIM4,1, 0);
+
+	// Lancement du compteur
+	MyTimer_Base_Start(TIM4);
+	
+}
